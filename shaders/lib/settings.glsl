47f1101f32aa1fb72d869c3561438c1698030d0f
Leo-Petersen <dp191252@umconnect.umt.edu>
2026-09-20 22:15:48 -0600

    Added bounceLight to lowest userpreset; takes barely any frames with the performance improvements for huge quality gain

--- shaders/lib/settings.glsl
+++ shaders/lib/settings.glsl
@@ -35,7 +35,6 @@
 
 //BounceLight//
     #define BounceLight
-        #define bounceQuality 4 //[2 3 4 5 6]
         #define BounceLightStr 1.00 //[0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.10 0.20 0.30 0.40 0.50 0.60 0.70 0.80 0.90 1.00 1.25 1.50 1.75 2.00]
 
 
